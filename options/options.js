b3065035758ad85c68801942e3a45a97d73f8ed6
Crowdbotics <team@crowdbotics.com>
2021-10-31 23:05:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "the_muse_mentorship_31778",
+  url: "https://the_muse_mentorship_31778.botics.co",
+  api: "https://the_muse_mentorship_31778.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
